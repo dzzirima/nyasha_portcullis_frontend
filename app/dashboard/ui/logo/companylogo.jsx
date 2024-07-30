4e7fb86d898ba770a4ca5f8bf703079df7f60b71
dzzirima <dzzirima@gmail.com>
2024-07-30 15:51:48 +0200

    feat: initial commit

---
+++ app/dashboard/ui/logo/companylogo.jsx
@@ -0,0 +1,13 @@
+import AddAlarmSharpIcon from '@mui/icons-material/AddAlarmSharp';
+
+export default function CompanyLogo() {
+ 
+  return (
+    <div
+      className={`flex flex-row items-center leading-none text-white`}
+    >
+      {/* <AddAlarmSharpIcon className="h-12 w-12 rotate-[15deg]" /> */}
+      <p className="text-[30px] "> portcullis</p>
+    </div>
+  );
+}
